d012cedbb2cb07e7912d84241c3cf5e1a0517bb9
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 15:39:51 -0300

    feat: Criado model Team

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,28 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Team',
+  timestamps: false,
+});
+
+export default Team;
