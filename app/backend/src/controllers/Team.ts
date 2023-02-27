b3cf77b51dc00c877f7ffe9ba2d253db1dd52b17
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 16:58:26 -0300

    feat: Get Team por id

--- app/backend/src/controllers/Team.ts
+++ app/backend/src/controllers/Team.ts
@@ -5,12 +5,12 @@ export default class TeamController {
   constructor(private teamService = new TeamService()) {}
 
   public async getAllTeams(_req: Request, res: Response): Promise<Response | void> {
-    try {
-      const result = await this.teamService.getAllTeams();
-      res.status(200).json(result);
-    } catch (error) {
-      const err = error as Error;
-      res.status(500).json({ message: err.message });
-    }
+    const result = await this.teamService.getAllTeams();
+    res.status(200).json(result);
+  }
+
+  public async getTeamById(req: Request, res: Response): Promise<Response | void> {
+    const result = await this.teamService.getTeamById(Number(req.params.id));
+    res.status(200).json(result);
   }
 }
