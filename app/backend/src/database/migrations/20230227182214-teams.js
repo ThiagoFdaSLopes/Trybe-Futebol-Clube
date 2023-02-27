782deac2acb2f55d024b76075ce63c1f811bc3fb
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 15:39:44 -0300

    fix: Descomentado seeders

---
+++ app/backend/src/database/migrations/20230227182214-teams.js
@@ -0,0 +1,21 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', 
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    team_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
