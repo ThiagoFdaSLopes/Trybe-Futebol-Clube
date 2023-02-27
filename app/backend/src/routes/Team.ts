0072d2dcc10c796f287985dec2f71593c2777907
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 16:58:35 -0300

    feat: Rota teams/:id

--- app/backend/src/routes/Team.ts
+++ app/backend/src/routes/Team.ts
@@ -6,5 +6,6 @@ const teamController = new TeamController();
 const teamRouter = Router();
 
 teamRouter.get('/', (req: Request, res: Response) => teamController.getAllTeams(req, res));
+teamRouter.get('/:id', (req: Request, res: Response) => teamController.getTeamById(req, res));
 
 export default teamRouter;
