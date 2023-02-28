1b1c3d522fb87d7029106b452fc10b2ae08feeac
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 11:06:19 -0300

    feat: Edit e Finish Matches

--- app/backend/src/controllers/Matche.ts
+++ app/backend/src/controllers/Matche.ts
@@ -14,4 +14,27 @@ export default class MatcheController {
     const result = await this.matcheService.getAllMatches(inProgress === 'true');
     res.status(200).json(result);
   }
+
+  public async finishMatche(req: Request, res: Response): Promise<Response | void> {
+    const { id } = req.params;
+
+    try {
+      await this.matcheService.finishMatche(Number(id));
+      res.status(200).json({ message: 'Finished' });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  public async EditMatches(req: Request, res: Response): Promise<Response | void> {
+    const { id } = req.params;
+    try {
+      await this.matcheService.EditMatche(Number(id), req.body);
+      res.status(200).json({ message: 'Placar Alterado' });
+    } catch (err) {
+      const error = err as Error;
+      res.status(500).json({ message: error.message });
+    }
+  }
 }
