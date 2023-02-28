022e836ffd9c7ddac6e2f35fd9e1aaaefe84febc
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 11:06:38 -0300

    feat: Routes Matches

--- app/backend/src/routes/Matche.ts
+++ app/backend/src/routes/Matche.ts
@@ -1,10 +1,21 @@
 import { Request, Response, Router } from 'express';
 import { MatcheController } from '../controllers';
+import validateToken from '../middleware/validateToken';
 
 const matcheController = new MatcheController();
 
 const matcheRouter = Router();
 
 matcheRouter.get('/', (req: Request, res: Response) => matcheController.getAllMatches(req, res));
+matcheRouter.patch(
+  '/:id',
+  validateToken,
+  (req: Request, res: Response) => matcheController.EditMatches(req, res),
+);
 
+matcheRouter.patch(
+  '/:id/finish',
+  validateToken,
+  (req: Request, res: Response) => matcheController.finishMatche(req, res),
+);
 export default matcheRouter;
