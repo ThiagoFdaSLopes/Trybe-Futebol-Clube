e0ffc04a6a0952dd22912dd17549c6533afdc7fd
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 22:38:29 -0300

    feat: ValidateUserLogin

---
+++ app/backend/src/services/validations/schema.ts
@@ -0,0 +1,26 @@
+import Joi = require('joi');
+import { IUser } from '../../interfaces/IUser';
+
+const errorMessage = 'All fields must be filled';
+
+const validateUserLogin = (body: IUser) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.empty': errorMessage,
+      'string.email': 'Invalid email or password',
+      'string.required': errorMessage,
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': errorMessage,
+      'string.min': 'Invalid email or password',
+      'string.required': errorMessage,
+    }),
+  });
+  const { error } = schema.validate(body);
+  if (error) {
+    const err = error as Error;
+    throw new Error(err.message);
+  }
+};
+
+export default validateUserLogin;
