efa0110868ec2e7ab4d04fd2fc8a22c11ea7a432
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 10:13:19 -0300

    feat: Filtro no getAllTeams

--- app/backend/src/controllers/Matche.ts
+++ app/backend/src/controllers/Matche.ts
@@ -4,8 +4,14 @@ import { MatcheService } from '../services';
 export default class MatcheController {
   constructor(private matcheService = new MatcheService()) {}
 
-  public async getAllMatches(_req: Request, res: Response): Promise<Response | void> {
-    const result = await this.matcheService.getAllMatches();
+  public async getAllMatches(req: Request, res: Response): Promise<Response | void> {
+    const { inProgress } = req.query;
+    if (inProgress === undefined) {
+      const result = await this.matcheService.getAllMatches();
+      return res.status(200).json(result);
+    }
+
+    const result = await this.matcheService.getAllMatches(inProgress === 'true');
     res.status(200).json(result);
   }
 }
--- app/backend/src/services/Matche.ts
+++ app/backend/src/services/Matche.ts
@@ -1,16 +1,26 @@
-import { ModelStatic } from 'sequelize';
+import { ModelStatic, Op } from 'sequelize';
 import Team from '../database/models/Team';
 import Matche from '../database/models/Matche';
 
 export default class MatcheService {
   protected model: ModelStatic<Matche> = Matche;
 
-  async getAllMatches(): Promise<Matche[]> {
+  async getAllMatches(inProgress?: boolean): Promise<Matche[]> {
+    if (inProgress === undefined) {
+      const result = await this.model.findAll(
+        { include: [
+          { model: Team, as: 'homeTeam', attributes: ['teamName'] },
+          { model: Team, as: 'awayTeam', attributes: ['teamName'] },
+        ] },
+      );
+      return result;
+    }
     const result = await this.model.findAll(
       { include: [
         { model: Team, as: 'homeTeam', attributes: ['teamName'] },
         { model: Team, as: 'awayTeam', attributes: ['teamName'] },
-      ] },
+      ],
+      where: { [Op.and]: [{ inProgress }] } },
     );
     return result;
   }
