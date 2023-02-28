67f5486436673f542442f6cd10e0725919a36573
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 17:29:07 -0300

    feat: Refatorando para usar away ou home

--- app/backend/src/controllers/LeaderBoard.ts
+++ app/backend/src/controllers/LeaderBoard.ts
@@ -4,8 +4,13 @@ import { LeaderBoardService } from '../services';
 export default class LeaderBoardController {
   constructor(private leaderBoardService = new LeaderBoardService()) {}
 
-  public async GetResults(_req: Request, res: Response): Promise<Response | void> {
-    const result = await this.leaderBoardService.GetResults();
+  public async GetResultsHome(_req: Request, res: Response): Promise<Response | void> {
+    const result = await this.leaderBoardService.GetResults('home');
+    res.status(200).json(result);
+  }
+
+  public async GetResultsAway(_req: Request, res: Response): Promise<Response | void> {
+    const result = await this.leaderBoardService.GetResults('away');
     res.status(200).json(result);
   }
 }
