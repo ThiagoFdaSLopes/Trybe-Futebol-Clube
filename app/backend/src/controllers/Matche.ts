2db8f0f6e414f866a5214edcf6746a5788bba756
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 12:01:40 -0300

    feat: Criado Matche com Validacoes

--- app/backend/src/controllers/Matche.ts
+++ app/backend/src/controllers/Matche.ts
@@ -26,4 +26,17 @@ export default class MatcheController {
     await this.matcheService.EditMatche(Number(id), req.body);
     res.status(200).json({ message: 'Placar Alterado' });
   }
+
+  public async CreateMatche(req: Request, res: Response): Promise<Response | void> {
+    try {
+      const result = await this.matcheService.CreateMatche(req.body);
+      res.status(201).json(result);
+    } catch (err) {
+      const error = err as Error;
+      if (error.message !== 'It is not possible to create a match with two equal teams') {
+        return res.status(404).json({ message: 'There is no team with such id!' });
+      }
+      res.status(422).json({ message: error.message });
+    }
+  }
 }
