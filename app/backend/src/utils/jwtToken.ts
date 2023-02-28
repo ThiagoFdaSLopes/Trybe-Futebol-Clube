8e2237005522625e0a04983beef1018ac76ed936
Thiago <programadorthiagolopes@gmail.com>
2023-02-27 23:12:28 -0300

    feat: Verify Token

--- app/backend/src/utils/jwtToken.ts
+++ app/backend/src/utils/jwtToken.ts
@@ -1,11 +1,15 @@
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
-import { IUser } from '../interfaces/IUser';
+import { IPayload } from '../interfaces/IPayload';
 
 dotenv.config();
 
-function createToken(payload: IUser): string {
+function createToken(payload: IPayload): string {
   return sign(payload, process.env.JWT_SECRET as string);
+}
+
+export function decodeToken(token: string) {
+  return verify(token, process.env.JWT_SECRET as string);
 }
 
 export default createToken;
