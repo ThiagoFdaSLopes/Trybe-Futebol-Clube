5708aa062a4c7a7cfb8ebde3f6604ee3dbdeaf1e
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 19:19:47 -0300

    feat: Rota para leaderboard completo

--- app/backend/src/controllers/LeaderBoard.ts
+++ app/backend/src/controllers/LeaderBoard.ts
@@ -13,4 +13,13 @@ export default class LeaderBoardController {
     const result = await this.leaderBoardService.GetResults('away');
     res.status(200).json(result);
   }
+
+  public async GetResults(_req: Request, res: Response): Promise<Response | void> {
+    const resultsHome = await this.leaderBoardService.GetResults('home');
+    const resultsAway = await this.leaderBoardService.GetResults('away');
+
+    const results = LeaderBoardService.GetLeaderBoard(resultsHome, resultsAway);
+
+    res.status(200).json(results);
+  }
 }
