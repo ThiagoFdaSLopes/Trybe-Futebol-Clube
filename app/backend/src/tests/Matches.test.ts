9af892d771493ce10968eacb8b27c29f746cc5f6
Thiago <programadorthiagolopes@gmail.com>
2023-02-28 12:06:51 -0300

    feat: Testes 90%

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -80,4 +80,46 @@ describe('Teste Matches', () => {
     expect(chaiHttpResponse.status).to.be.deep.equal(200);
   });
 
+  it('Testando o metodo patch rota matches para criar matche id nao existe', async () => {
+    chaiHttpResponse = await chai
+       .request(app).post('/matches')
+        .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2Nzc1OTA1NDF9.QXwSdQYZKCYzLWS7njCAU4Xf46KhRsm3hbCVVVsyNqI')
+        .send({
+          "homeTeamId": 12,
+          "awayTeamId": 6465654,
+          "homeTeamGoals": 2,
+          "awayTeamGoals": 2
+        });
+
+    expect(chaiHttpResponse.status).to.be.deep.equal(404);
+  });
+
+  it('Testando o metodo patch rota matches para criar matche id iguais', async () => {
+    chaiHttpResponse = await chai
+       .request(app).post('/matches')
+        .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2Nzc1OTA1NDF9.QXwSdQYZKCYzLWS7njCAU4Xf46KhRsm3hbCVVVsyNqI')
+        .send({
+          "homeTeamId": 12,
+          "awayTeamId": 12,
+          "homeTeamGoals": 2,
+          "awayTeamGoals": 2
+        });
+
+    expect(chaiHttpResponse.status).to.be.deep.equal(422);
+  });
+
+  it('Testando o metodo patch rota matches para criar matche valida', async () => {
+    chaiHttpResponse = await chai
+       .request(app).post('/matches')
+        .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsInBhc3N3b3JkIjoiJDJhJDA4JHhpLkh4azFjekFPMG5aUi4uQjM5M3UxMGFFRDBSUTFOM1BBRVhRN0h4dExqS1BFWkJ1LlBXIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJpYXQiOjE2Nzc1OTA1NDF9.QXwSdQYZKCYzLWS7njCAU4Xf46KhRsm3hbCVVVsyNqI')
+        .send({
+          "homeTeamId": 12,
+          "awayTeamId": 13,
+          "homeTeamGoals": 2,
+          "awayTeamGoals": 2
+        });
+
+    expect(chaiHttpResponse.status).to.be.deep.equal(201);
+  });
+
 });
